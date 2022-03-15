6b60482d48c4bf58727d104c850aa150359c1b6f
Unknown <rudsonmartina@gmail.com>
2022-03-16 00:16:41 +0100

    else-statement
    1.3

--- meten-is-weten/main.py
+++ meten-is-weten/main.py
@@ -1,7 +1,9 @@
-a = int(input("voer een getal in voor a1"))
-b = int(input("voer een getal in voor b1"))
+a = int(input("voer een getal in voor a1: "))
+b = int(input("voer een getal in voor b1: "))
 
 if a > b:
     print("a is het grootste getal: {}".format(a))
 elif a < b:
     print("a is het kleinste getal: {}".format(a))
+else:
+    print("a en b zijn even groot")
