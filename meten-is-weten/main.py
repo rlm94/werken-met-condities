032084ab3e782cf7f0c10fb922ef730d6283d209
Unknown <rudsonmartina@gmail.com>
2022-03-16 09:19:11 +0100

    min-en-max
    1.4

--- meten-is-weten/main.py
+++ meten-is-weten/main.py
@@ -2,8 +2,12 @@ a = int(input("voer een getal in voor a1: "))
 b = int(input("voer een getal in voor b1: "))
 
 if a > b:
-    print("a is het grootste getal: {}".format(a))
+    print(f"a is het grootste getal: {a}")
+    print(f"Het minimum is: {b}")
+    print(f"Het maximum is: {a}")
 elif a < b:
-    print("a is het kleinste getal: {}".format(a))
+    print(f"a is het kleinste getal: {a}")
+    print(f"Het minimum is: {a}")
+    print(f"Het maximum is: {b}")
 else:
     print("a en b zijn even groot")
